b968f1127cb63656c807940be104404880eb5f62
Marcin Staszek <marcin.staszek88@gmail.com>
2019-03-25 17:50:57 +0100

    initial commit

---
+++ src/main/java/pl/ms/SafeDriving/SafeDrivingApplication.java
@@ -0,0 +1,13 @@
+package pl.ms.SafeDriving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeDrivingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeDrivingApplication.class, args);
+	}
+
+}
