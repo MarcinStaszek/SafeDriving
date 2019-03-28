c7e808f2e043c54f3ff319b57e2c6d15d9d15fbc
Marcin Staszek <marcin.staszek88@gmail.com>
2019-03-28 23:47:34 +0100

    Add spring security initial config, home page access to all users

--- src/main/java/pl/ms/SafeDriving/controller/HomeController.java
+++ src/main/java/pl/ms/SafeDriving/controller/HomeController.java
@@ -1,16 +1,15 @@
 package pl.ms.SafeDriving.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-
-    @RequestMapping("/")
+    @GetMapping("/")
     public String home() {
         return "home";
     }
-
 
 }
